032bb0e06cb8ec48924e7b5645dda72268134600
pipo <psychedelic.nekopunch@gmail.com>
2015-08-26 22:54:55 +0900

    add service

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/// <reference path="typings/angular2/angular2.d.ts" />
 if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
     switch (arguments.length) {
@@ -10,21 +9,32 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
 if (typeof __metadata !== "function") __metadata = function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+/// <reference path="typings/angular2/angular2.d.ts" />
 var angular2_1 = require('angular2/angular2');
+var Messages = (function () {
+    function Messages() {
+        this.test = 'success';
+    }
+    Messages.prototype.get = function () {
+        return this.test;
+    };
+    return Messages;
+})();
 // Annotation section
 var MyAppComponent = (function () {
-    function MyAppComponent() {
+    function MyAppComponent(message) {
         this.title = 'masa69';
+        this.mes = message.get();
     }
     MyAppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app'
         }),
         angular2_1.View({
-            templateUrl: 'templates/app.html'
+            template: "\n\t\t<h1>{{ title }}</h1>\n\t\t<p>{{ mes }}</p>\n\t"
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [Messages])
     ], MyAppComponent);
     return MyAppComponent;
 })();
-angular2_1.bootstrap(MyAppComponent);
+angular2_1.bootstrap(MyAppComponent, [Messages]);
--- app.ts
+++ app.ts
@@ -1,22 +1,44 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
-
 import {Component, View, bootstrap} from 'angular2/angular2';
+
+class Messages
+{
+	// private first: string;
+	test: string;
+
+	constructor()
+	{
+		this.test = 'success';
+	}
+
+	get()
+	{
+		return this.test;
+	}
+}
 
 // Annotation section
 @Component({
 	selector: 'my-app'
 })
 @View({
-	templateUrl: 'templates/app.html'
+	template: `
+		<h1>{{ title }}</h1>
+		<p>{{ mes }}</p>
+	`
 })
+
 // Component controller
 class MyAppComponent
 {
-	title: string;
+	title : string;
+	mes   : string;
 
-	constructor() {
+	constructor(message: Messages)
+	{
 		this.title = 'masa69';
+		this.mes   = message.get()
 	}
 }
 
-bootstrap(MyAppComponent);
+bootstrap(MyAppComponent, [Messages]);
